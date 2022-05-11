95595b2ee3b673f6d0edc5eaa2288cf8e791108e
Crisss <69833083+cgcris09@users.noreply.github.com>
2022-05-11 00:24:15 -0500

    Add files via upload

---
+++ lab14/lab_13.php
@@ -0,0 +1,80 @@
+<!--  I honor Parkland's core values by affirming that I have 
+followed all academic integrity guidelines for this work.
+Cristal Cardenas
+201H -->
+
+
+<html>
+<head>
+<title>PHP Session</title>
+<h3>Login Page</h3>
+Please enter username and password: <br>
+
+
+
+
+<?php
+// this code is from kurban/login.php
+require("lib/phpfunctions.php");
+
+session_start();
+
+$message="";
+$username = getPost('155username');
+$password = getPost('155password');
+if (isset($_POST['submit']))
+{
+    if ($_POST['submit'] == 'Login')
+    {
+    if (validate_login($username, $password))
+    { 
+        $_SESSION['username'] = $username;
+        header('Location: welcome.php');
+    }
+    $message = "Invalid username or password";
+    }
+}  
+
+
+
+
+?>
+</head>
+<body>
+<form method='POST'>
+<b>Username:</b> <input type='text' name='155username' value='<?php showPost("155username");?>'> <br>
+<b>Password:</b> <input type='password' name='155password' value='<?php showPost("155password");?>'> <br>
+<input type='submit' name='submit' value='Login'> <br>
+<div style='position: absolute; bottom: 10px; '><p><?php echo $message;?></p></div>
+username is hello, password is world
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
